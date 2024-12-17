28b5c0ce74f2e8385fd9013ac6e9e09cdb9657d1
minjeong9919 <minjeong9919@naver.com>
2024-12-17 21:13:11 +0900

    refactor: 버튼들을 플라스틱 material로 변환

--- camera/components/imageSelector.js
+++ camera/components/imageSelector.js
@@ -1,77 +1,75 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import { plastic } from "../texture/plastic.js";
 
 export const imageSelector = () => {
+  const plasticMaterial = plastic();
+  const extrudeSettings = {
+    depth: 0.02,
+    bevelEnabled: true,
+    bevelThickness: 0.01,
+    bevelSize: 0.01,
+    bevelOffset: 0,
+    bevelSegments: 10,
+  };
 
-	const extrudeSettings = {
-		depth: 0.02,
-		bevelEnabled: true,
-		bevelThickness: 0.01,
-		bevelSize: 0.01,
-		bevelOffset: 0,
-		bevelSegments: 10
-	};
+  const buttonGroup = new THREE.Group();
 
-	const material = new THREE.MeshStandardMaterial( { color: '#ddd', side: THREE.DoubleSide } );
-	const buttonMaterial = new THREE.MeshStandardMaterial( { color: '#3d3d3d', side: THREE.DoubleSide } );
+  const containerShape = new THREE.Shape();
+  containerShape.ellipse(0, 0, 0.25, 0.25);
 
-	const buttonGroup = new THREE.Group();
+  const containerGeometry = new THREE.ExtrudeGeometry(
+    containerShape,
+    extrudeSettings
+  );
+  const container = new THREE.Mesh(containerGeometry, plasticMaterial);
+  container.position.x = -1;
+  container.position.y = -0.3;
+  container.position.z = -0.53;
+  buttonGroup.add(container);
 
-	const containerShape = new THREE.Shape();
-	containerShape.ellipse( 0, 0, 0.25, 0.25 );
+  const arrowShape = new THREE.Shape();
+  arrowShape.moveTo(0, 0.07);
+  arrowShape.lineTo(-0.07, 0.07);
+  arrowShape.lineTo(0, 0);
+  arrowShape.lineTo(0.07, 0.07);
+  arrowShape.lineTo(0, 0.07);
 
-	const containerGeometry = new THREE.ExtrudeGeometry( containerShape, extrudeSettings );
-	const container = new THREE.Mesh( containerGeometry, material );
-	container.position.x = - 1;
-	container.position.y = - 0.3;
-	container.position.z = - 0.53;
-	buttonGroup.add( container );
+  const geometry = new THREE.ExtrudeGeometry(arrowShape, extrudeSettings);
+  const up = new THREE.Mesh(geometry, plasticMaterial);
+  up.position.y = -0.1;
+  up.position.x = -1;
+  up.position.z = -0.56;
+  up.rotateZ(Math.PI);
 
-	const arrowShape = new THREE.Shape();
-	arrowShape.moveTo( 0, 0.07 );
-	arrowShape.lineTo( - 0.07, 0.07 );
-	arrowShape.lineTo( 0, 0 );
-	arrowShape.lineTo( 0.07, 0.07 );
-	arrowShape.lineTo( 0, 0.07 );
+  const right = up.clone();
+  right.position.x = -1.2;
+  right.position.y = -0.3;
+  right.rotateZ(Math.PI / 2);
 
-	const geometry = new THREE.ExtrudeGeometry( arrowShape, extrudeSettings );
-	const up = new THREE.Mesh( geometry, buttonMaterial );
-	up.position.y = - 0.1;
-	up.position.x = - 1;
-	up.position.z = - 0.56;
-	up.rotateZ( Math.PI );
+  const down = up.clone();
+  down.position.y = -0.5;
+  down.position.x = -1;
+  down.rotateZ(Math.PI);
 
-	const right = up.clone();
-	right.position.x = - 1.2;
-	right.position.y = - 0.3;
-	right.rotateZ( Math.PI / 2 );
+  const left = up.clone();
+  left.position.x = -0.8;
+  left.position.y = -0.3;
+  left.rotateZ((Math.PI * 3) / 2);
 
-	const down = up.clone();
-	down.position.y = - 0.5;
-	down.position.x = - 1;
-	down.rotateZ( Math.PI );
+  const circleShape = new THREE.Shape();
+  circleShape.ellipse(0, 0, 0.05, 0.05);
 
-	const left = up.clone();
-	left.position.x = - 0.8;
-	left.position.y = - 0.3;
-	left.rotateZ( Math.PI * 3 / 2 );
+  const enterGeometry = new THREE.ExtrudeGeometry(circleShape, extrudeSettings);
+  const enter = new THREE.Mesh(enterGeometry, plasticMaterial);
+  enter.position.z = -0.56;
+  enter.position.y = -0.3;
+  enter.position.x = -1;
 
+  buttonGroup.add(up);
+  buttonGroup.add(right);
+  buttonGroup.add(down);
+  buttonGroup.add(left);
+  buttonGroup.add(enter);
 
-	const circleShape = new THREE.Shape();
-	circleShape.ellipse( 0, 0, 0.05, 0.05 );
-
-	const enterGeometry = new THREE.ExtrudeGeometry( circleShape, extrudeSettings );
-	const enter = new THREE.Mesh( enterGeometry, buttonMaterial );
-	enter.position.z = - 0.56;
-	enter.position.y = - 0.3;
-	enter.position.x = - 1;
-
-
-	buttonGroup.add( up );
-	buttonGroup.add( right );
-	buttonGroup.add( down );
-	buttonGroup.add( left );
-	buttonGroup.add( enter );
-
-	return buttonGroup;
-
+  return buttonGroup;
 };
--- camera/components/smallButtons.js
+++ camera/components/smallButtons.js
@@ -1,54 +1,54 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 export const smallButtons = () => {
+  const buttonMaterial = new THREE.MeshStandardMaterial({
+    color: "#3d3d3d",
+    side: THREE.DoubleSide,
+  });
 
-	const buttonMaterial = new THREE.MeshStandardMaterial( { color: '#3d3d3d', side: THREE.DoubleSide } );
-	const extrudeSettings = {
-		depth: 0.02,
-		bevelEnabled: true,
-		bevelThickness: 0.01,
-		bevelSize: 0.01,
-		bevelOffset: 0,
-		bevelSegments: 10
-	};
+  const extrudeSettings = {
+    depth: 0.02,
+    bevelEnabled: true,
+    bevelThickness: 0.01,
+    bevelSize: 0.01,
+    bevelOffset: 0,
+    bevelSegments: 10,
+  };
 
-	const buttonGroup = new THREE.Group();
+  const buttonGroup = new THREE.Group();
 
-	const circleShape = new THREE.Shape();
-	circleShape.ellipse( 0, 0, 0.05, 0.05 );
+  const circleShape = new THREE.Shape();
+  circleShape.ellipse(0, 0, 0.05, 0.05);
 
-	const enterGeometry = new THREE.ExtrudeGeometry( circleShape, extrudeSettings );
-	const enter = new THREE.Mesh( enterGeometry, buttonMaterial );
-	enter.position.z = - 0.56;
-	enter.position.y = - 0.3;
-	enter.position.x = - 1;
+  const enterGeometry = new THREE.ExtrudeGeometry(circleShape, extrudeSettings);
+  const enter = new THREE.Mesh(enterGeometry, buttonMaterial);
+  enter.position.z = -0.56;
+  enter.position.y = -0.3;
+  enter.position.x = -1;
 
-	const smallButton1 = enter.clone();
-	smallButton1.position.z = - 0.53;
-	smallButton1.position.y = 0.65;
-	smallButton1.position.x = - 0.8;
+  const smallButton1 = enter.clone();
+  smallButton1.position.z = -0.53;
+  smallButton1.position.y = 0.5;
+  smallButton1.position.x = -0.8;
 
-	const smallButton2 = smallButton1.clone();
-	smallButton2.position.y = 0.4;
+  const smallButton2 = smallButton1.clone();
+  smallButton2.position.y = 0.3;
 
-	const smallButton3 = smallButton1.clone();
-	smallButton3.position.y = 0.15;
+  const smallButton3 = smallButton1.clone();
+  smallButton3.position.y = 0.1;
 
-	const smallButton4 = smallButton1.clone();
-	smallButton4.position.y = - 0.68;
+  const smallButton4 = smallButton1.clone();
+  smallButton4.position.y = -0.68;
 
-	const smallButton5 = smallButton1.clone();
-	smallButton5.position.y = - 0.68;
-	smallButton5.position.x = - 1.2;
+  const smallButton5 = smallButton1.clone();
+  smallButton5.position.y = -0.68;
+  smallButton5.position.x = -1.2;
 
-	buttonGroup.add( smallButton1 );
-	buttonGroup.add( smallButton2 );
-	buttonGroup.add( smallButton3 );
-	buttonGroup.add( smallButton4 );
-	buttonGroup.add( smallButton5 );
+  buttonGroup.add(smallButton1);
+  buttonGroup.add(smallButton2);
+  buttonGroup.add(smallButton3);
+  buttonGroup.add(smallButton4);
+  buttonGroup.add(smallButton5);
 
-
-
-	return buttonGroup;
-
+  return buttonGroup;
 };

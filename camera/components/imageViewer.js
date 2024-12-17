74fc0d36381c249d7ac52126e5e65d021a37eef2
minjeong9919 <minjeong9919@naver.com>
2024-12-17 21:14:14 +0900

    refactor: 위치 수정

--- camera/components/imageViewer.js
+++ camera/components/imageViewer.js
@@ -1,15 +1,24 @@
-import * as THREE from 'three';
-import { createBoxWithRoundedEdges } from '../shapes/createBoxWithRoundedEdge.js';
+import * as THREE from "three";
+import { createBoxWithRoundedEdges } from "../shapes/createBoxWithRoundedEdge.js";
+import { env } from "../texture/env.js";
 
 export const imageViewer = () => {
+  const textureLoader = new THREE.TextureLoader();
+  const map = textureLoader.load("../../public/images/profile.jpg");
+  const envMap = env();
+  map.repeat.set(0.5, 0.5);
 
-	const material = new THREE.MeshStandardMaterial( { color: '#ddd', side: THREE.DoubleSide } );
-	const geometry = createBoxWithRoundedEdges( 1.8, 1.5, 0.05, 0.05, 1 );
-	const mesh = new THREE.Mesh( geometry, material );
-	mesh.position.y = 0;
-	mesh.position.x = 0.3;
-	mesh.position.z = - 0.5;
+  const material = new THREE.MeshStandardMaterial({
+    color: "#ddd",
+    side: THREE.DoubleSide,
+    map: map,
+    envMap: envMap,
+  });
+  const geometry = createBoxWithRoundedEdges(1.8, 1.3, 0.05, 0.05, 1);
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.y = -0.05;
+  mesh.position.x = 0.3;
+  mesh.position.z = -0.5;
 
-	return mesh;
-
+  return mesh;
 };
--- camera/components/viewFinder/viewFinder.js
+++ camera/components/viewFinder/viewFinder.js
@@ -1,19 +1,18 @@
-import * as THREE from 'three';
-import { container } from './container.js';
-import { viewFinderLens } from './viewFinderLens.js';
+import * as THREE from "three";
+import { container } from "./container.js";
+import { viewFinderLens } from "./viewFinderLens.js";
 
 const viewFinderObject = new THREE.Object3D();
 
 export const viewFinder = () => {
+  const Lens = viewFinderLens();
+  const Container = container();
 
-	const Lens = viewFinderLens();
-	const Container = container();
+  viewFinderObject.add(Lens);
+  viewFinderObject.add(Container);
 
-	viewFinderObject.add( Lens );
-	viewFinderObject.add( Container );
+  viewFinderObject.position.y = 0.6;
+  viewFinderObject.position.x = 0.3;
 
-	viewFinderObject.position.y = 0.6;
-
-	return viewFinderObject;
-
+  return viewFinderObject;
 };

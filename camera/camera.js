c96fb09329b2ea630be1ccf8602644af5b1f4d67
minjeong9919 <minjeong9919@naver.com>
2024-12-17 21:10:02 +0900

    feat: GLT 파일 변환 기능 추가

--- camera/camera.js
+++ camera/camera.js
@@ -1,141 +1,147 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'OrbitControls';
-import { viewFinder } from './components/viewFinder/viewFinder.js';
-import { lens } from './components/lens.js';
-import { shutter } from './components/shutter.js';
-import { imageViewer } from './components/imageViewer.js';
-import { imageSelector } from './components/imageSelector.js';
-import { smallButtons } from './components/smallButtons.js';
-import { body } from './components/body.js';
+import * as THREE from "three";
+import { OrbitControls } from "OrbitControls";
+import { viewFinder } from "./components/viewFinder/viewFinder.js";
+import { lens } from "./components/lens.js";
+import { shutter } from "./components/shutter.js";
+import { imageViewer } from "./components/imageViewer.js";
+import { imageSelector } from "./components/imageSelector.js";
+import { smallButtons } from "./components/smallButtons.js";
+import { body } from "./components/body.js";
+import { GLTFExporter } from "https://unpkg.com/three@0.138.0/examples/jsm/exporters/GLTFExporter.js";
+
+window.onload = function () {
+  const app = new App();
+
+  document.querySelector("#export-button").addEventListener("click", () => {
+    app._exportToGLTF();
+    console.log("내보내기 완료");
+  });
+};
 
 class App {
+  constructor() {
+    const divContainer = document.querySelector("#webgl-container");
+    this._divContainer = divContainer; // 다른 메서드에서 참조 가능하도록 하기 위함
 
-	constructor() {
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    divContainer.appendChild(renderer.domElement);
+    this._renderer = renderer;
 
-		const divContainer = document.querySelector( '#webgl-container' );
-		this._divContainer = divContainer; // 다른 메서드에서 참조 가능하도록 하기 위함
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color("#DFDEDE");
+    this._scene = scene;
 
-		const renderer = new THREE.WebGLRenderer( { antialias: true } );
-		renderer.setPixelRatio( window.devicePixelRatio );
-		divContainer.appendChild( renderer.domElement );
-		this._renderer = renderer;
+    this._setupCamera();
+    this._setupLight();
+    this._setupModel();
+    this._setupControls();
 
-		const scene = new THREE.Scene();
-		scene.background = new THREE.Color( '#DFDEDE' );
-		this._scene = scene;
+    document.querySelector("#export-button").addEventListener("click", () => {
+      this._exportToGLTF();
+      console.log("내보내기 완료");
+    });
 
-		this._setupCamera();
-		this._setupLight();
-		this._setupModel();
-		this._setupControls();
+    // '_'로 시작하는 것은 해당 class 내에서만 사용되는 Private 이라고 지정
+    // 개발자들간의 약속임!
+    window.onresize = this.resize.bind(this);
+    this.resize();
+    // 창크키가 변경될 때마다 창 크기에 맞게 설정되도록
+    // this가 app class의 객체를 가리키도록 bind로 묶어줌.
+    requestAnimationFrame(this.render.bind(this));
+  }
 
-		// '_'로 시작하는 것은 해당 class 내에서만 사용되는 Private 이라고 지정
-		// 개발자들간의 약속임!
-		window.onresize = this.resize.bind( this );
-		this.resize();
-		// 창크키가 변경될 때마다 창 크기에 맞게 설정되도록
-		// this가 app class의 객체를 가리키도록 bind로 묶어줌.
-		requestAnimationFrame( this.render.bind( this ) );
+  _setupControls() {
+    new OrbitControls(this._camera, this._divContainer);
+  }
 
-	}
+  _setupCamera() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+    camera.position.z = 7;
+    this._camera = camera;
+  }
 
-	_setupControls() {
+  _setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(0, 7, -6.6);
+    this._scene.add(light);
 
-		new OrbitControls( this._camera, this._divContainer );
+    const ambientIntensity = 0.5;
+    const ambientLight = new THREE.AmbientLight(0xffffff, ambientIntensity);
+    this._scene.add(ambientLight);
+  }
 
-	}
+  _setupModel() {
+    const camera = new THREE.Object3D();
+    this._scene.add(camera);
 
-	_setupCamera() {
+    const Body = body(0.5, 0.1);
+    camera.add(Body);
 
-		const width = this._divContainer.clientWidth;
-		const height = this._divContainer.clientHeight;
-		const camera = new THREE.PerspectiveCamera(
-			75,
-			width / height,
-			0.1,
-			100
-		);
-		camera.position.z = 7;
-		this._camera = camera;
+    const ViewFinder = viewFinder();
+    camera.add(ViewFinder);
 
-	}
+    const lensMesh = lens();
+    camera.add(lensMesh);
 
-	_setupLight() {
+    const buttonGeometry = shutter();
+    camera.add(buttonGeometry);
 
-		const color = 0xffffff;
-		const intensity = 1;
-		const light = new THREE.DirectionalLight( color, intensity );
-		light.position.set( 5, 10, 7.5 );
-		this._scene.add( light );
+    const ImageViewer = imageViewer();
+    camera.add(ImageViewer);
 
-		const ambientIntensity = 0.5;
-		const ambientLight = new THREE.AmbientLight( 0xffffff, ambientIntensity );
-		this._scene.add( ambientLight );
+    const ImageSelector = imageSelector();
+    camera.add(ImageSelector);
 
-	}
+    const SmallButton = smallButtons();
+    camera.add(SmallButton);
+  }
 
-	_setupModel() {
+  _exportToGLTF() {
+    const exporter = new GLTFExporter();
 
-		const camera = new THREE.Object3D();
-		this._scene.add( camera );
+    exporter.parse(
+      this._scene,
+      (gltf) => {
+        const blob = new Blob([JSON.stringify(gltf)], {
+          type: "application/json",
+        });
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "camera_model.glb";
+        link.click();
+      },
+      { binary: false }
+    );
+  }
 
-		const Body = body( 0.5, 0.1 );
-		camera.add( Body );
+  resize() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
 
-		const ViewFinder = viewFinder();
-		camera.add( ViewFinder );
+    this._camera.aspect = width / height;
+    this._camera.updateProjectionMatrix();
 
-		const lensMesh = lens();
-		camera.add( lensMesh );
+    this._renderer.setSize(width, height);
+  }
 
+  render(time) {
+    this._renderer.render(this._scene, this._camera);
+    this.update(time);
+    requestAnimationFrame(this.render.bind(this));
+  }
 
-		const buttonGeometry = shutter();
-		camera.add( buttonGeometry );
-
-		const ImageViewer = imageViewer();
-		camera.add( ImageViewer );
-
-		const ImageSelector = imageSelector();
-		camera.add( ImageSelector );
-
-
-		const SmallButton = smallButtons();
-		camera.add( SmallButton );
-
-	}
-
-	resize() {
-
-		const width = this._divContainer.clientWidth;
-		const height = this._divContainer.clientHeight;
-
-		this._camera.aspect = width / height;
-		this._camera.updateProjectionMatrix();
-
-		this._renderer.setSize( width, height );
-
-	}
-
-	render( time ) {
-
-		this._renderer.render( this._scene, this._camera );
-		this.update( time );
-		requestAnimationFrame( this.render.bind( this ) );
-
-	}
-
-	update( time ) {
-
-		time *= 0.001;
-		// this._cube.rotation.x = time;
-		// this._cube.rotation.y = time;
-
-	}
-
+  update(time) {
+    time *= 0.001;
+    // this._cube.rotation.x = time;
+    // this._cube.rotation.y = time;
+  }
 }
 
 window.onload = function () {
-
-	new App();
-
+  new App();
 };
